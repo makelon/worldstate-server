c8684aac833c750ebb7099378254d89b445f8c94
makelon <makelon@tenno.tools>
2019-05-09 11:00:40 +0200

    Create empty itemnames.json and itemtypes.json if necessary

--- build.js
+++ build.js
@@ -341,6 +341,12 @@ if (opt.data && !(fs.existsSync(path.join(dataDir, itemNames)) && fs.existsSync(
 	})
 	rl.question('Continue anyway? [yN] ', (answer) => {
 		if (answer.toLowerCase() == 'y') {
+			for (const fileName of [itemNames, itemTypes]) {
+				const filePath = path.join(dataDir, fileName);
+				if (!fs.existsSync(filePath)) {
+					fs.writeFileSync(filePath, '{}', 'utf8');
+				}
+			}
 			build()
 		}
 		rl.close()
